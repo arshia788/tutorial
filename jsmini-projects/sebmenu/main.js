56296cb19aed9da756c1e0e2e0365be50b972837
Amirali Amanipour <arshia.ala80@gmail.com>
2022-06-18 13:23:11 +0430

    adding my js projects

---
+++ jsmini-projects/sebmenu/main.js
@@ -0,0 +1,13 @@
+const menu= document.querySelector('.profile')
+menu.addEventListener('click', event=>{
+       let x= document.querySelector('.menu')
+       x.classList.toggle('active')
+       
+})
+
+
+
+
+
+
+
